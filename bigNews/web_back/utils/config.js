8e50087699a80f4d5ac0178c931bd0b8fa6c3455
XXXieChenHao <492316578@qq.com>
2019-10-14 01:44:53 +0800

    <feat>:<新增登录、退出功能，完成基本信息获取>

---
+++ bigNews/web_back/utils/config.js
@@ -0,0 +1,12 @@
+// config - 中文含义为配置
+// - 我们可以将项目中使用的接口地址统一保存在这个文件中
+
+var baseUrl = 'http://localhost:8000';
+
+
+var USER_LOGIN = baseUrl + '/admin/login'; // 登录接口
+var USER_LOGOUT = baseUrl + '/admin/logout'; // 退出接口
+var USER_INFO = baseUrl + '/admin/getuser'; // 用户简单信息获取接口
+
+var USER_INFO_GET = baseUrl + '/admin/userinfo_get'; // 用户详细信息获取接口
+var USER_INFO_EDIT = baseUrl + '/admin/userinfo_edit'; // 用户信息编辑接口
---
+++ bigNews/web_back/utils/user.js
@@ -0,0 +1,45 @@
+var user = {
+
+  // 登陆页面
+  login: obj => {
+    $.ajax({
+      type: 'post',
+      url: USER_LOGIN,
+      data: obj.data,
+      success: obj.callback
+    })
+  },
+
+  // 退出操作
+  logout: obj => {
+    $.ajax({
+      type: 'post',
+      url: USER_LOGOUT,
+      data: obj.data,
+      success: obj.callback
+    })
+  },
+
+  // 获取基本信息
+  getInfo: obj => {
+    $.ajax({
+      type: 'get',
+      url: USER_INFO,
+      data: obj.data,
+      success: obj.callback
+    })
+  },
+
+  getAllInfo: obj => {
+    $.ajax({
+      type: 'get',
+      url: USER_INFO_GET,
+
+    })
+  }
+
+
+
+
+
+}
